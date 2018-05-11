37e744170cf3391d28870cce27a6d3783cf7f8ab
Darren Moore <darren.m@firecreekweb.com>
2018-05-11 20:19:23 +0700

    Added example config file data

---
+++ config.example.js
@@ -0,0 +1,14 @@
+var config = {};
+
+config.gi = {
+  name: 'test',
+  secret: 'NrCgyKqvyB',
+  host: 'http://localhost:3000'
+};
+
+config.user = {
+  name: 'good-intentions-user'
+};
+
+
+module.exports = config;
